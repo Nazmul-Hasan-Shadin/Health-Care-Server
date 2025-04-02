48b3751ebb77b08942db270c7351e9025e701ad7
Nazmul-Hasan-Shadin <nazmulhasanshadin000@gmail.com>
2025-04-03 01:48:13 +0600

    admin endpoint has added

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
-import express, { Request, Response } from 'express'
+import express, { Request, Response, urlencoded } from 'express'
 import cors from 'cors'
+import { UserRoutes } from './app/modules/User/user.route';
 
 const app=express();
 app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 app.use(cors({
      origin:'*'
 }))
@@ -10,4 +12,5 @@ app.use(cors({
 app.get('/',(req:Request,res:Response)=>{
     res.send('app is listening at 3000')
 })
+app.use('/api/v1',UserRoutes)
 export default app
---
+++ src/app/modules/User/user.services.ts
@@ -0,0 +1,27 @@
+import {  PrismaClient, UserRole } from "@prisma/client";
+const prisma=new PrismaClient();
+
+const createAdmin=async(data:any)=>{
+    console.log(data);
+    const userData={
+        email:data.admin.email,
+        password:data.password,
+        role:UserRole.ADMIN
+    }
+    const result=await prisma.$transaction(async(transactionClient)=>{
+        const createdUser= await transactionClient.user.create({
+            data:userData
+        })
+        const createdAdminData=await transactionClient.admin.create({
+            data:data.admin
+        })
+        return createdAdminData
+    })
+
+  
+    
+    return result
+}
+export const userService={
+    createAdmin
+}
